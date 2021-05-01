8954af79d5738f082985c23424067159296bbfac
trackhang1909 <trackhang1909@gmail.com>
2021-05-02 02:06:12 +0700

    Index, call, account detail view

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+function addFriend (event) {
+    if (event.className === 'btn btn-secondary btn-sm') {
+        event.parentNode.innerHTML = '<button onclick="addFriend(this)" type="button" class="btn btn-danger btn-sm">Hủy kết bạn</button>'
+    }
+    else {
+        event.parentNode.innerHTML = '<button onclick="addFriend(this)" type="button" class="btn btn-secondary btn-sm">Kết bạn</button>';
+    }
+}
