010b508347936babd163dd9e983b2f2cda0de8a0
kd2t <48654294+duyum1133@users.noreply.github.com>
2021-06-07 20:22:30 +0700

    Update avatar

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,16 +1,50 @@
 let socket = io()
 
+
 $(document).ready(function () {
+    let friendsIdArr = [];
+
+    // get id of list friends
+    $('.friends').each(function (i, e) {
+        friendsIdArr.push(e.id);
+    });
+
+    console.log(friendsIdArr);
+
+    let fromId = $('.fromUser').attr('id');
+
+    if (socket.connect) {
+        console.log('socket.io is connected.')
+        if (fromId) {
+            socket.emit("Client-sent-data", fromId);
+        }
+    }
+
     $('#callBtn').on('click', function (e) {
         alert('click')
     })
 
     socket.on("Server-sent-data", function (data) {
-        let fromId = $('.fromUser').attr('id');
         if (data.id_user == fromId || data == fromId) {
             $('#right-panel').load('/account-detail .right-panel-content')
             $('.navbar').load('/ #nav-content')
             $('#middle-panel').load('/account-detail .middle-panel-content')
         }
+
+        friendsIdArr.forEach(element => {
+            if (element == data) {
+
+                $('#' + element).append("<span class='c-avatar__status'></span>")
+                // $('#' + element).children(".c-avatar__status").css("display", "none")
+
+                console.log('zzzz');
+            }
+
+
+        });
     });
+
+    // if (socket.disconnect) {
+    //     isOnlineArr = isOnlineArr.filter(item => item !== fromId)
+    // }
 })
