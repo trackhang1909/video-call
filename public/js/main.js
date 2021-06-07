ab16c32a11af9ef744395f1c9287d0fd8a1134f1
trackhang1909 <trackhang1909@gmail.com>
2021-06-07 10:56:32 +0700

    Add loading ajax

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+$("#loading").hide();
+
 function addFriend(event) {
     if (event.className === 'btn btn-secondary btn-sm') {
         event.parentNode.innerHTML = '<button onclick="addFriend(this)" type="button" class="btn btn-danger btn-sm">Hủy kết bạn</button>'
