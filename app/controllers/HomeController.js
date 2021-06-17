0ed5efb7f45a13dd459804daf995a7c91c576c6b
kd2t <48654294+duyum1133@users.noreply.github.com>
2021-06-17 20:54:26 +0700

    Change password

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -3,6 +3,9 @@ const Notification = require("../models/Notification");
 const Suggest = require("../models/Suggest");
 const User = require("../models/User");
 const CallLog = require("../models/CallLog");
+const { validationResult } = require('express-validator');
+const saltRounds = 10;
+const bcrypt = require('bcrypt')
 
 let title
 
@@ -104,6 +107,65 @@ class HomeController {
             return res.render('account-detail', { title, isLogged, user, list_suggest, list_notification, userId, callLogs });
         }
         return res.render('account-detail', { isLogged });
+    }
+
+    changePasswordGet = async (req, res) => {
+        title = 'Đổi mật khẩu'
+        let error = req.flash('error') || ''
+        let success = req.flash('success') || ''
+        const token = req.cookies.token
+
+        let isLogged = req.flash('isLogged') || false
+
+        if (token) {
+            isLogged = req.flash('isLogged', true)
+
+            let payload = verify(token, 'secret')
+            let userId = payload.id
+            global.userId = userId
+
+            let user = await User.findById(userId)
+
+            let list_notification = await Notification.findOne({ id_user: user }).populate('request_from.user')
+
+            return res.render('change-password', { title, isLogged, user, list_notification, error, success });
+        }
+        return res.render('change-password', { title, isLogged });
+    }
+
+    changePasswordPost = async (req, res) => {
+        title = 'Đổi mật khẩu'
+        // get result of validation
+        let result = validationResult(req)
+
+        // get password / confirm_password from form
+        let password = req.body.password
+        let userId = req.body.user_id
+
+        // if validation success
+        if (result.errors.length === 0) {
+            const hash_password = bcrypt.hashSync(password, saltRounds);
+
+            await User.findByIdAndUpdate(userId, { password: hash_password })
+
+            req.flash("success", 'Đổi mật khẩu thành công.')
+            return res.redirect('/change-password')
+        }
+        // if validation fail
+        else {
+            result = result.mapped();
+            let msg
+            let fields
+
+            // save first message error
+            for (fields in result) {
+                msg = result[fields].msg;
+                break;
+            }
+
+            req.flash("error", msg + '.')
+            return res.redirect('/change-password')
+        }
     }
 }
 
--- routes/home.js
+++ routes/home.js
@@ -9,5 +9,9 @@ router.get('/', HomeController.index);
 router.get('/call', HomeController.call);
 // [GET] /account-detail
 router.get('/account-detail', authMiddleware.isLogged, HomeController.accountDetail);
+// [GET] /change-password
+router.get('/change-password', authMiddleware.isLogged, HomeController.changePasswordGet);
+// [POST] /change-password
+router.post('/change-password', authMiddleware.changePassword, HomeController.changePasswordPost);
 
 module.exports = router;
